b149bfaab04c05863497d1a3a4e2053738b00250
Sai Koushik Emani <evnskoushik@gmail.com>
2025-03-19 23:22:12 +0530

    initial commit

---
+++ src/main/java/com/orange/flavor_quartz_job/FlavorQuartzJobApplication.java
@@ -0,0 +1,13 @@
+package com.orange.flavor_quartz_job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlavorQuartzJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlavorQuartzJobApplication.class, args);
+	}
+
+}
